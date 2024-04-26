4b71c6d7afb077384a76f3fc88340f5c177118a6
Doston Togaev <togayevd37@gmail.com>
2024-04-27 02:35:50 +0500

    2-commit

--- fibo.py
+++ fibo.py
@@ -15,19 +15,7 @@ class FibonacciIterator:
         self.a, self.b = self.b, self.a + self.b
         return result
 
-def fibonacci_generator(limit):
-    a, b = 0, 1
-    while limit > 0:
-        yield a
-        a, b = b, a + b
-        limit -= 1
-
 print("Iterator:")
 fib_iter = FibonacciIterator(20)
 for num in fib_iter:
-    print(num, end=" ")
-print("\n")
-
-print("Generator:")
-for num in fibonacci_generator(20):
     print(num, end=" ")
